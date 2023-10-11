9e9e2fa49f7ed787b5a6c33682b1758b70bce3b4
Eli <eli.serra173@gmail.com>
2023-10-11 03:42:13 +0200

    add vite config files

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: './docs',
+    emptyOutDir: true
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: './test/setup.js'
+  }
+})
