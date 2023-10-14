36977618405f23799837cff07a5c7f02e61dfaf9
Eli <eli.serra173@gmail.com>
2023-10-14 04:28:11 +0200

    Improve tests and add some new test

--- test/utils.test.js
+++ test/utils.test.js
@@ -52,10 +52,34 @@ test('test isStr method', () => {
     expect(ut.isStr("1")).toBe(true)
     expect(ut.isStr("Hello world")).toBe(true)
     expect(ut.isStr(`hello`)).toBe(true)
+    expect(ut.isStr(``)).toBe(true)
     expect(ut.isStr(0)).toBe(false)
     expect(ut.isStr(-10)).toBe(false)
     expect(ut.isStr({a: 1})).toBe(false)
     expect(ut.isStr([2, 3])).toBe(false)
+})
+test('test isStrNotEmpty method', () => {
+    expect(ut.isStrNotEmpty("1")).toBe(true)
+    expect(ut.isStrNotEmpty("Hello world")).toBe(true)
+    expect(ut.isStrNotEmpty(`hello`)).toBe(true)
+    expect(ut.isStrNotEmpty(``)).toBe(false)
+    expect(ut.isStrNotEmpty(0)).toBe(false)
+    expect(ut.isStrNotEmpty(-10)).toBe(false)
+    expect(ut.isStrNotEmpty({a: 1})).toBe(false)
+    expect(ut.isStrNotEmpty([2, 3])).toBe(false)
+})
+
+test('test isKey method', () => {
+    expect(ut.isKey("h1")).toBe(true)
+    expect(ut.isKey("Hello_world")).toBe(true)
+    expect(ut.isKey(`Hello_W0r_ld`)).toBe(true)
+    expect(ut.isKey(`hello_world_a1`)).toBe(true)
+    expect(ut.isKey(`_hello_world`)).toBe(false)
+    expect(ut.isKey(`hello_world_`)).toBe(false)
+    expect(ut.isKey(0)).toBe(false)
+    expect(ut.isKey(-10)).toBe(false)
+    expect(ut.isKey({a: 1})).toBe(false)
+    expect(ut.isKey([2, 3])).toBe(false)
 })
 
 test('test isAttrKey method', () => {
@@ -69,6 +93,12 @@ test('test isAttrKey method', () => {
     expect(ut.isAttrKey(-10)).toBe(false)
     expect(ut.isAttrKey({a: 1})).toBe(false)
     expect(ut.isAttrKey([2, 3])).toBe(false)
+})
+
+test('test isValidRegex method', () => {
+    expect(ut.isValidRegex(/[0-9]/)).toBe(true)
+    expect(ut.isValidRegex('/[0-9]/')).toBe(true)
+    expect(ut.isValidRegex('hello world')).toBe(true)
 })
 
 test('test isElement method', () => {
