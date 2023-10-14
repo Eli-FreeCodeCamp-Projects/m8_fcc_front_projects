b15cb896de8645cdac5ae713aa692b257d0555e2
Eli <eli.serra173@gmail.com>
2023-10-14 18:00:43 +0200

    Add Root React rooter page

---
+++ src/pages/root.jsx
@@ -0,0 +1,23 @@
+import {Link, NavLink, Outlet} from "react-router-dom";
+import {Breadcrumb} from "./breadcrumb.jsx";
+import {RootNavBar} from "./navBar.jsx";
+
+/**
+ * View home from router
+ * @return {JSX.Element}
+ * @constructor
+ */
+export function Root(){
+
+
+    return(
+        <div className="app-main">
+            <header className="bg-dark app-header">
+                <RootNavBar />
+            </header>
+            <Breadcrumb />
+            <div className='container-fluid p-2 app-content'><Outlet /></div>
+        </div>
+
+    )
+}
