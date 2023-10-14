63ad694037d56f903abc4539809687858ab6e4fd
Eli <eli.serra173@gmail.com>
2023-10-14 17:46:07 +0200

    Add React router pages Sass

---
+++ src/assets/projects.jsx
@@ -0,0 +1,55 @@
+/**
+ *
+ */
+export const projects = [
+    {
+        name: 'Random Quote Machine',
+        slug: 'random-quote-machine',
+        showTitle: true,
+        parent: 'front-end-projects',
+        link: '/front-end-projects/random-quote-machine',
+        img: '/img/RandomQuoteMachine.png',
+        imgAlt: 'Random Quote Machine React App',
+        description: '(React, Sass and Javascript)'
+    },
+    {
+        name: 'MarkDown Previewer',
+        slug: 'markdown-previewer',
+        showTitle: false,
+        parent: 'front-end-projects',
+        link: '/front-end-projects/markdown-previewer',
+        img: '/img/MarkdownPreviewer.png',
+        imgAlt: 'MarkDown Previewer React App',
+        description: '(React, Sass, Bootstrap and Javascript)'
+    },
+    {
+        name: 'Drum Machine',
+        slug: 'drum-machine',
+        showTitle: true,
+        parent: 'front-end-projects',
+        link: '/front-end-projects/drum-machine',
+        img: '/img/DrumMachine.png',
+        imgAlt: 'Drum Machine React App',
+        description: '(React, Sass, Bootstrap and Javascript)'
+    },
+    {
+        name: 'JavaScript Calculator',
+        slug: 'js-calculator',
+        showTitle: true,
+        parent: 'front-end-projects',
+        link: '/front-end-projects/js-calculator',
+        img: null,
+        imgAlt: 'JavaScript Calculator React App',
+        description: '(React, Sass, Bootstrap and Javascript)'
+    },
+    {
+        name: '25+5 Clock',
+        slug: '25-5-clock',
+        showTitle: true,
+        parent: 'front-end-projects',
+        link: '/front-end-projects/25-5-clock',
+        img: null,
+        imgAlt: '25+5 Clock React App',
+        description: '(React, Sass, Bootstrap and Javascript)'
+    }
+]
