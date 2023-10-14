31cf9e7d3079140bf3f2d1e836a85dda57e3c356
Eli <eli.serra173@gmail.com>
2023-10-14 18:00:53 +0200

    Add Home React rooter page

---
+++ src/pages/home.jsx
@@ -0,0 +1,17 @@
+
+/**
+ * View home from router
+ * @return {JSX.Element}
+ * @constructor
+ */
+export function Home(){
+    return(
+        <>
+            <section id="welcome-section">
+                <h1>FreeCodeCamp Certification Projects</h1>
+                <p>By Eli Serra</p>
+            </section>
+        </>
+
+    )
+}
